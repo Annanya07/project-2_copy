bad33e8b6c764b72fff07a5a2c27cec6948b99fd
Annanya <annanyadwivedi1@gmail.com>
2019-06-15 09:56:55 -0500

    adding files

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/listings2.sqlite"
+db = SQLAlchemy(app)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+# @app.route("/project_analysis", methods=['GET', 'POST'])
+# def project_analysis():
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/logic.js
@@ -0,0 +1,24 @@
+
+// Creating map object
+var map = L.map("map", {
+    center: [41.8486645, -87.7355111],
+    zoomsnap: 10
+  });
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 500,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(map);
+
+
+// Our style object
+var mapStyle = {
+    color: "white",
+    fillColor: "salmon",
+    fillOpacity: 0.5,
+    weight: 1.5
+  };
+
