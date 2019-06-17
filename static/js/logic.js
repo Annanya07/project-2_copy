cdffc5b6739111fd95f23599f0596b14db9a2ef3
Terry D <46873532+devereux44@users.noreply.github.com>
2019-06-17 18:52:21 -0500

    adding maps and templates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,17 +1,16 @@
-// Creating map object
-var map = L.map("map", {
-    center: [41.8486645, -87.7355111],
-    zoom: 10
-  })
+// Create a map object
+var myMap = L.map("map", {
+  center: [41.87, -87.62],
+  zoom: 10
+});
 
-// Adding tile layer
+// Add a tile layer to the map
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 500,
+  mapZoom: 12,
   id: "mapbox.streets",
   accessToken: API_KEY
-}).addTo(map);
-
+}).addTo(myMap);
 
 // Our style object
 var mapStyle = {
@@ -21,3 +20,28 @@ var mapStyle = {
     weight: 1.5
   };
 
+  function createMarkers(response) {
+
+    // Pull the "stations" property off of response.data
+    var listings = response.data.stations;
+  
+    // Initialize an array to hold bike markers
+    var listMarkers = [];
+  
+    // Loop through the stations array
+    for (var index = 0; index < listings.length; index++) {
+      var listing = listings[index];
+  
+      // For each station, create a marker and bind a popup with the station's name
+      var listMarker = L.marker([listing.latitude, listing.longitude])
+        .bindPopup("<h3>" + listing.id + "<h3><h3>Rating: " + listing.rating + "<h3><h3>Revenue: " + listing.price);
+  
+      // Add the marker to the bikeMarkers array
+      listMarkers.push(listMarker);
+    }
+  
+    // Create a layer group made from the bike markers array, pass it into the createMap function
+    createMap(L.layerGroup(listMarkers));
+  }
+
+d3.csv("../Resources/listings2.csv", createMarkers)
